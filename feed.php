30d8c3135665343cb2160271933e27ca4e4d9866
sponrad <sponrad@gmail.com>
2014-02-27 21:32:20 -0800

    feed link to restaurants

--- feed.php
+++ feed.php
@@ -6,6 +6,7 @@
   <div class="row">
     <div class="col-md-4 phone-contain">
       <h1>Feed</h1>
+      <a href="/restaurants">Nearby Restaurants</a>
       <!--  <button id="voteButton">Vote</button> --> 
       <button id="menuButton">Menu</button>
     </div>
