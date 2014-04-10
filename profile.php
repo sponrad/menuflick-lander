d5e1ea3c714ffa52098848cdf535c8b1a3bad1dd
sponrad <sponrad@gmail.com>
2014-04-10 11:23:31 -0700

    profile follow unfollow

--- profile.php
+++ profile.php
@@ -15,6 +15,8 @@ else {
   <div class="row">
     <div class="col-md-8 phone-contain">
       <h1 id="userName">Profile</h1>
+      <div id="isfollowing"></div>
+
       <p>Following: <span id="following"></span></p>
       <p>Followers: <span id="followers"></span></p>
       <p>Reviews: <span id="reviews"></span></p>
@@ -45,9 +47,46 @@ else {
 	 $("#following").html( data.following );
 	 $("#followers").html( data.followers );
 	 $("#reviews").html( data.reviewcount );
-	 
+	 if (data.isfollowing == true){
+	   $("#isfollowing").html("<button id='unFollowButton'>Unfollow</button>");
+	 }
+	 else {
+	   $("#isfollowing").html("<button id='followButton'>Follow</button>");
+	 }	 
        }
      }
+   });
+
+   //follow
+   $("#isfollowing").on("click", "#followButton", function(e){
+     e.preventDefault();
+     $.ajax({
+       url: "http://mfbackend.appspot.com/json/followuser",
+       dataType: "json",
+       data: dataToSend,
+       type: "GET",
+       success: function(data){
+	 console.log(data);
+	 if (data.response == 1){
+	   $("#isfollowing").html("<button id='unFollowButton'>Unfollow</button>");   
+	 }}
+     });
+   });
+
+   //unfollow
+   $("#isfollowing").on("click", "#unFollowButton", function(e){
+     e.preventDefault();
+     $.ajax({
+       url: "http://mfbackend.appspot.com/json/unfollowuser",
+       dataType: "json",
+       data: dataToSend,
+       type: "GET",
+       success: function(data){
+	 console.log(data);
+	 if (data.response == 1){
+	   $("#isfollowing").html("<button id='followButton'>Follow</button>");   
+	 }}
+     });
    });
 
 
