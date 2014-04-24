f852ff27491dbba0c45657cb122c23dbad59768f
sponrad <sponrad@gmail.com>
2014-04-23 21:33:06 -0700

    user reviews to profile page

--- profile.php
+++ profile.php
@@ -20,6 +20,11 @@ else {
       <p>Following: <span id="following"></span></p>
       <p>Followers: <span id="followers"></span></p>
       <p>Reviews: <span id="reviews"></span></p>
+
+      <div id="reviewDiv">
+	<div id="fakeReview"></div>
+      </div>
+
     </div>
   </div>
 </div>
@@ -28,6 +33,38 @@ else {
  $(document).ready( function(){
    
    var profileId = <?= $profileId; ?>;
+
+   function convertRating(rating){
+     switch(rating)
+     {
+       case 100:
+	 return "+"
+	 break;
+       case 50:
+	 return "/"
+	 break;
+       case 0:
+	 return "-"
+	 break;
+     }
+   }
+
+   function parsePrompt(data){
+     var html = "<div class='reviewItem'>";
+     console.log(data);
+     html += convertRating(data.rating) + " ";
+     html += "<a class='profilelink' href='/profile?profileid="+data.userid+"'>" + data.username + "</a><br>";
+     
+     html += data.prompt
+		 .replace("{{input}}", "<span style='display: inline; color:red;'>"+data.input+"</span>")
+		 .replace("{{restaurant}}", "<a style='display: inline;' href='/items?restaurantid="+ data.restaurantid +"'>"+data.restaurant+"</a>")
+		 .replace("{{dish}}", "<a style='display: inline;'>"+data.item+"</a>")
+		 .replace("{{input2}}", "<span type='text' name='input2' style='display: inline; color: red;'>"+data.input2+"</span>");
+
+     html += "</div><br>";
+
+     return html;
+   }
 
    dataToSend = {
      userid: '<?= $userId; ?>',
@@ -49,10 +86,18 @@ else {
 	 $("#reviews").html( data.reviewcount );
 	 if (data.isfollowing == true){
 	   $("#isfollowing").html("<button id='unFollowButton'>Unfollow</button>");
+	   if (dataToSend.userid == dataToSend.profileid){
+	     $("#isfollowing").html("");
+	   }
 	 }
 	 else {
 	   $("#isfollowing").html("<button id='followButton'>Follow</button>");
 	 }	 
+
+	 for (var key in data.feed_items){
+	   var html = parsePrompt(data.feed_items[key]);
+	   $("#fakeReview").after( html );
+	 }	
        }
      }
    });
